0c49ef6b265f642e835790308f3fd136e79c3269
Me-ann Urgel <meann1_2@yahoo.com>
2019-05-02 14:48:25 +0800

    drop down browser default

--- src/Components/recipe/NewRecipe.js
+++ src/Components/recipe/NewRecipe.js
@@ -1,96 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NewRecipe = () => {
-  return (
-    <div className="container row center">
-      <form className="col s12 m6 offset-m3">
+class NewRecipe extends Component {
 
-        <h1 className="header">Create New recipe</h1>
-        <div className="input-field">
-          <label htmlFor="recipeName" />
-          <input type="text" placeholder="Recipe Name" id="recipeName" />
-        </div>
-        <div className="input-field">
-          <label htmlFor="description" />
-          <textarea className="materialize-textarea" id="description" placeholder="Description"></textarea>
-        </div>
-        <div className="input-field row">
-          <div className="col s12 m10">
+  state = {
+    recipeName: "",
+    description: "",
+    instructions: "",
+    difficulty: "",
+    prepTime: "",
+    cookTime: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    })
+  }
+
+
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state)
+
+  }
+
+  render() {
+
+    return (
+      <div className="container row center">
+        <form className="col s12 m8 offset-m2" onSubmit={this.handleSubmit}>
+
+          <h1 className="header">Create New recipe</h1>
+          <div className="input-field">
+            <label htmlFor="recipeName" />
+            <input type="text" onChange={this.handleChange} placeholder="Recipe Name" id="recipeName" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="description" />
+            <textarea className="materialize-textarea" onChange={this.handleChange} id="description" placeholder="Description"></textarea>
+          </div>
+          <div className="input-field">
             <label htmlFor="instructions" />
-            <textarea className="materialize-textarea" id="instructions" placeholder="Instructions (click add button to add instructions)"></textarea>
+            <textarea className="materialize-textarea" onChange={this.handleChange} id="instructions" placeholder="Instructions"></textarea>
           </div>
-          <div className="col s12 m2">
-            <a href="/" className="btn btn-floating circle prefix"><i className="material-icons">add</i></a>
-          </div>
-        </div>
-        <div className="row difficulty" id="difficulty">
-          <div className="col m4">
-            <label htmlFor="easy">
-              <input className="with-gap" value="easy" type="radio" />
-              <span>Easy </span>
-            </label>
-          </div>
-          <div className="col m2">
-            <label htmlFor="intermediate">
-              <input className="with-gap" value="intermediate" type="radio" />
-              <span>Intermediate </span>
-            </label>
-          </div>
-          <div className="col m4 offset-m1">
-            <label htmlFor="hard">
-              <input className="with-gap" value="hard" type="radio" />
-              <span>Hard </span>
-            </label>
-          </div>
-        </div>
-        <div className="input-field row">
-          <div className="col s12 m5" >
-            <select id="prepTime">
-              <option value="" disabled>Prep Time</option>
-              <option value="10 mins">10 mins</option>
-              <option value="30 mins">30 mins</option>
-              <option value="45 mins">45 mins</option>
-              <option value="1 hour">1 hour</option>
-              <option value="1.5 hour">1.5 hour</option>
-              <option value="2 hours">2 hours</option>
-              <option value="more than 2 hours">more than 2 hours</option>
-            </select>
-            <label htmlFor="prepTime"></label>
-          </div>
-          <div className="col s12 m5" >
-            <select id="cookTime">
-              <option value="" disabled>Cook Time</option>
-              <option value="10 mins">10 mins</option>
-              <option value="30 mins">30 mins</option>
-              <option value="45 mins">45 mins</option>
-              <option value="1 hour">1 hour</option>
-              <option value="1.5 hour">1.5 hour</option>
-              <option value="2 hours">2 hours</option>
-              <option value="3 hours">3 hours</option>
-              <option value="8 hours">8 hours</option>
-              <option value="more than 8 hours">more than 8 hours</option>
-            </select>
-            <label htmlFor="cookTime"></label>
-          </div>
+          <br />
+          <div className="row">
+            <div className="col s12 m4" >
+              <label htmlFor="difficulty"> Level of Difficulty
+              <select className="browser-default" id="difficulty" onChange={this.handleChange}>
+                  <option value="easy">Easy</option>
+                  <option value="intermediate">Intermediate</option>
+                  <option value="hard">Hard</option>
+                </select>
+              </label>
+            </div>
+            <div className="col s12 m4" >
+              <label htmlFor="prepTime">Prep Time</label>
+              <select className="browser-default" id="prepTime" onChange={this.handleChange}>
+                <option value="5 mins">5 mins</option>
+                <option value="10 mins">10 mins</option>
+                <option value="30 mins">30 mins</option>
+                <option value="45 mins">45 mins</option>
+                <option value="1 hour">1 hour</option>
+                <option value="1.5 hour">1.5 hour</option>
+                <option value="2 hours">2 hours</option>
+                <option value="more than 2 hours">more than 2 hours</option>
+              </select>
 
-        </div>
-        <div className="file-field input-field row">
+            </div>
+            <div className="col s12 m4" >
+              <label htmlFor="cookTime">Cook Time</label>
+              <select className="browser-default" id="cookTime" onChange={this.handleChange}>
+                <option value="10 mins">10 mins</option>
+                <option value="30 mins">30 mins</option>
+                <option value="45 mins">45 mins</option>
+                <option value="1 hour">1 hour</option>
+                <option value="1.5 hour">1.5 hour</option>
+                <option value="2 hours">2 hours</option>
+                <option value="3 hours">3 hours</option>
+                <option value="8 hours">8 hours</option>
+                <option value="more than 8 hours">more than 8 hours</option>
+              </select>
 
-          <div className="file-path-wrapper col s12 m8">
-            <input className="file-path validate" type="text" />
-          </div>
-          <div className="btn col s12 m4">
-            <span>Upload Image URL</span>
-            <input type="file" id="recipeImg" />
-          </div>
-        </div>
-        <div>
-          <button type="submit" className="btn light-blue accent-4">Submit Recipe</button>
+            </div>
 
-        </div>
-      </form>
-    </div>
-  );
+          </div>
+          <div>
+            <button type="submit" className="btn light-blue accent-4">Submit Recipe</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default NewRecipe;
